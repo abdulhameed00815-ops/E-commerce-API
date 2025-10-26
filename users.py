b693da8acf388b823c2db56408479bb0ed97ac8e
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2025-10-26 14:36:53 +0300

    error loging in (wrong password when its correct)

--- users.py
+++ users.py
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 
 fastapi = FastAPI()
 
-engine = create_engine("postgresql+asyncpg://postgres:1234@localhost/users")
+engine = create_engine("postgresql://postgres:1234@localhost/users")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 session = SessionLocal()
@@ -20,12 +20,19 @@ class User(Base):
     password = Column(String)
 
 
-class UserCreate(User):
-    pass
+Base.metadata.create_all(bind=engine)
+    
+
+class UserLogin(BaseModel):
+    email:str
+    username:str
+    password:str
 
 
-class UserLogin(User):
-    pass
+class UserCreate(BaseModel):
+    email:str
+    username:str
+    password:str
 
 
 def get_db():
@@ -36,19 +43,21 @@ def get_db():
         db.close()
 
 
-@fastapi.post('/')
-async def user_create(user: UserCreate, db: Session = Depends(get_db)):
-    if session.query(User).filter(User.email == user.email).first():
+@fastapi.post('/signup/')
+def user_create(user: UserCreate, db: Session = Depends(get_db)):
+    if session.query(User.email).filter(User.email == user.email).first():
         raise HTTPException(status_code=404, detail="user already exists")
-    new_user = User(User.id == user.id, User.email == user.email, User.username == user.username, User.password == user.password)
+    new_user = User(email = user.email, username = user.username, password = user.password)
+    db.add(new_user)
     db.commit()
     return {"user successfuly created!"}
 
-@fastapi.post('/')
-async def user_login(user: UserLogin, db: Session = Depends(get_db)):
+
+@fastapi.post('/signin/')
+def user_login(user: UserLogin, db: Session = Depends(get_db)):
     if not session.query(User).filter(User.email == user.email).first():
-        return {"user not found!"}
+        raise HTTPException(status_code=404, detail="user not found!")
     if not session.query(User.password).filter(User.username == user.username).first() == user.password:
-        return {"wrong password!"}
+        raise HTTPException(status_code=404, detail="wrong password!")
     user_name = session.query(User.username).filter(User.email == user.email).first()
     return {f"welcome {user_name}!"}
