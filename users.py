7d41482cbfbff4ea9ca37a7188d0b466cf36a67b
abdulhameed00815-ops <abdulhameed00815@gmail.com>
2025-11-07 19:20:09 +0200

    added refresh tokens + switched to low level AuthJWT, untested

--- users.py
+++ users.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session, relationship 
 from pydantic import BaseModel
-from auth_handler import sign_jwt
 import bcrypt
-from auth_bearer import JWTBearer, IsAdmin
-from auth_handler import decode_jwt
-
+from fastapi.responses import JSONResponse
+from fastapi_jwt_auth2 import AuthJWT
+from fastapi_jwt_auth2.exceptions import AuthJWTException
+from decouple import config
 fastapi = FastAPI()
 
 engine_users = create_engine("postgresql://postgres:1234@localhost/users")
@@ -35,6 +35,25 @@ class User(Base):
 
 
 Base.metadata.create_all(bind=engine_users)
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+
+class Settings(BaseModel):
+    authjwt_secret_key: str = JWT_SECRET
+
+
+@AuthJWT.load_config
+def get_config():
+    return Settings()
+
+
+@fastapi.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+        )
 
 
 class UserLogin(BaseModel):
@@ -72,33 +91,48 @@ def get_db_carts():
 
 
 @fastapi.post('/signup/')
-def user_create(user: UserCreate, db: Session = Depends(get_db_users)):
+def user_create(user: UserCreate, db: Session = Depends(get_db_users), Authorize: AuthJWT = Depends()):
     if db.query(User).filter(User.email == user.email).first():
         raise HTTPException(status_code=400, detail="user already exists")
+    user_id = db.query(User.id).filter(User.email == user.email).scalar()
     hashed_pw = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
     new_user = User(email = user.email, username = user.username, password = hashed_pw.decode('utf-8'))
 #we firstly encode the password given by the user in the create user, and store that encoded thing in a variable (hashed_pw), then when we come to add it to our db we decode it    
     db.add(new_user)
     db.commit()
     role1 = db.query(User.role).filter(User.email == user.email).first() 
-    return sign_jwt(user_id=user.email, role=role1[0])
-
+    access_token = Authorize.create_access_token(subject=user_id, user_claims={"email": user.email, "role": role1})
+    refresh_token = Authorize.create_refresh_token(subject=user_id, user_claims={"email": user.email, "role": role1})
+    return {"access_token": access_token, "refresh_token": refresh_token}
 
 @fastapi.post('/signin/')
-def user_login(user: UserLogin, db: Session = Depends(get_db_users)):
+def user_login(user: UserLogin, db: Session = Depends(get_db_users), Authorize: AuthJWT = Depends()):
     db_user = db.query(User).filter(User.email == user.email).first()
     if not db.query(User).filter(User.email == user.email).first():
         raise HTTPException(status_code=404, detail="user not found!")
+    user_id = db.query(User.id).filter(User.email == user.email).scalar()
     passkey = db.query(User.password).filter(User.email == user.email).first()
     if bcrypt.checkpw(user.password.encode('utf-8'), db_user.password.encode('utf-8')):
 #we now take the password entered by the user in the sign in process, then we use the checkpw function by bcrypt to see if the encoded version of the user password matches the encoded version of the password in our db        
         role1 = db.query(User.role).filter(User.email == user.email).scalar() 
-        role1 = str(role1).strip() 
-        return sign_jwt(user_id=user.email, role=role1)
+        role1 = str(role1).strip()
+        access_token = Authorize.create_access_token(subject=user_id, user_claims={"email": user.email, "role": role1})
+        refresh_token = Authorize.create_refresh_token(subject=user_id, user_claims={"email": user.email, "role": role1})
+        return {"access_token": access_token, "refresh_token": refresh_token}
 #we return the encoded token to the user once he signs up or in, this token will be later used to access secured routes
     else:
         raise HTTPException(status_code=401, detail="incorrect password!")
     
+
+@fastapi.post('/refresh/')
+def refresh(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_refresh_token_required()
+    claims = Authorize.get_raw_jwt()
+    email = claims.get("email")
+    role = claims.get("role")
+    new_access_token = Authorize.create_access_token(user_claims={"email": email, "role": role})
+    return {"access token": new_access_token}
+
 
 class Product(Base):
     __tablename__ = "products"
@@ -123,8 +157,18 @@ class ProductResponse(BaseModel):
     price:int
 
 
-@fastapi.post('/addproduct/', dependencies=[Depends(IsAdmin())], tags=["products"])
-def product_create(product: ProductCreate, db: Session = Depends(get_db_products)):
+def user_role(Authorize:AuthJWT = Depends()):
+    claims = Authorize.get_raw_jwt()
+    role = claims.get("role")
+    return role
+
+
+@fastapi.post('/addproduct/', tags=["products"])
+def product_create(product: ProductCreate, db: Session = Depends(get_db_products), Authorize: AuthJWT = Depends(), role: str = Depends(user_role)):
+    if role == "admin":
+        Authorize.jwt_required()
+    else:
+        raise HTTPException(status_code=403, detail="access denied bitch!")
     if db.query(Product).filter(Product.name == product.name).first():
         raise HTTPException(status_code=404, detail="product already exists!")
     new_product = Product(name = product.name, description = product.description, price = product.price)
@@ -133,8 +177,9 @@ def product_create(product: ProductCreate, db: Session = Depends(get_db_products
     return {"product added!"}
 
 
-@fastapi.get('/displayproducts/{Product.id}', dependencies=[Depends(JWTBearer())], response_model=ProductResponse, tags=["products"])
-def display_products(product_id:int, db: Session = Depends(get_db_products)):
+@fastapi.get('/displayproducts/{Product.id}', response_model=ProductResponse, tags=["products"])
+def display_products(product_id:int, db: Session = Depends(get_db_products), Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
     product = db.query(Product).filter(Product.id == product_id).first()
     if not product:
         raise HTTPException(status_code=404, detail="product already exists!")
@@ -164,16 +209,16 @@ class RemoveProductFromCart(BaseModel):
     product_id:int
     
 
-#this function gets the token from the jwtbearer class, then decodes it and extracts the email of the CURRENT user.
-async def get_current_user(request: Request, token: str = Depends(JWTBearer())):
-    payload = decode_jwt(token)
-    if not payload:
-        raise HTTPException(status_code=403, detail="token invalid or expired!")
-    return payload.get('id')
+def user_email(Authorize:AuthJWT = Depends()):
+    claims = Authorize.get_raw_jwt()
+    email = claims.get("email")
+    return email
+
 
 #endpoint for adding a product to cart
-@fastapi.post('/addtocart/', dependencies=[Depends(JWTBearer())], tags=["cart"])
-def add_to_cart(product: AddToCart, db_carts: Session = Depends(get_db_carts), db_products: Session = Depends(get_db_products), email: str = Depends(get_current_user)):
+@fastapi.post('/addtocart/', tags=["cart"])
+def add_to_cart(product: AddToCart, db_carts: Session = Depends(get_db_carts), db_products: Session = Depends(get_db_products), email: str = Depends(user_email), Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required() 
     product_id1 = db_products.query(Product.id).filter(Product.id == product.product_id).scalar()
     if not product_id1:
         raise HTTPException(status_code=404, detail="product not found!")
@@ -183,8 +228,9 @@ def add_to_cart(product: AddToCart, db_carts: Session = Depends(get_db_carts), d
     return {"product added to cart successfuly!"}
         
 #endpoint for viewing stuff in cart
-@fastapi.get('/viewcart/{Cart.id}', dependencies=[Depends(JWTBearer())], tags=["cart"])
-def view_cart(email: str = Depends(get_current_user), db_carts: Session = Depends(get_db_carts), db_products: Session = Depends(get_db_products)):
+@fastapi.get('/viewcart/{Cart.id}', tags=["cart"])
+def view_cart(email: str = Depends(user_email), db_carts: Session = Depends(get_db_carts), db_products: Session = Depends(get_db_products), Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required() 
     product_ids = [p[0] for p in db_carts.query(Cart.product_id).filter(Cart.user_id == email).all()]
     if not product_ids:
         return {"message": "your cart is empty!"}
@@ -193,8 +239,9 @@ def view_cart(email: str = Depends(get_current_user), db_carts: Session = Depend
     return {"cart products": product_names}
 
 
-@fastapi.put('/removeproductfromcart/', dependencies=[Depends(JWTBearer())], tags=["cart"])
-def remove_product(product: RemoveProductFromCart, email: str = Depends(get_current_user), db_carts: Session = Depends(get_db_carts)):
+@fastapi.put('/removeproductfromcart/', tags=["cart"])
+def remove_product(product: RemoveProductFromCart, email: str = Depends(user_email), db_carts: Session = Depends(get_db_carts), Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required() 
     desired_product = db_carts.query(Cart).filter(Cart.product_id == product.product_id, Cart.user_id == email).first()
     if not desired_product:
         raise HTTPException(status_code=404, detail="product unavailable!")
